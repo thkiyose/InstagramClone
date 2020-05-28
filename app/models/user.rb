fec373471ecad79a143cb493f0daf04e486b920f
Haruka Teduka <th_kiyose@outlook.com>
2020-05-28 09:59:00 +0900

    ユーザー登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_many :posts
+  validates :name, presence:true, length: { maximum: 30 }
+  validates :email, presence:true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/ },
+                    uniqueness: true
+  validates :password, presence:true, length: { minimum: 6 }
+  before_validation { email.downcase! }
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :posts, only:[:index,:new]
+  resources :users, only:[:new,:create,:show]
   root to: "posts#index"
 end
