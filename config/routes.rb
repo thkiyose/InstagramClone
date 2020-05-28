cfd29a5023267a891b53b0b239eed637773d9525
Haruka Teduka <th_kiyose@outlook.com>
2020-05-28 11:05:47 +0900

    新規投稿機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only:[:index,:new]
+  resources :posts, only:[:index,:new,:create]
   resources :users, only:[:new,:create,:show]
   root to: "posts#index"
 end
