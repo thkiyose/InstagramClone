275f4d2cf78bed3a6bd00225ac4b9c4000008bd0
Haruka Teduka <th_kiyose@outlook.com>
2020-05-28 14:10:22 +0900

    ログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only:[:index,:new,:create]
   resources :users, only:[:new,:create,:show]
+  resources :sessions, only:[:new,:create,:destroy]
   root to: "posts#index"
 end
