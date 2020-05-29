8b50c1a038b3b716f3df2f30eb3908dfd36142fa
Haruka Teduka <th_kiyose@outlook.com>
2020-05-29 15:58:53 +0900

    レイアウト調整

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,10 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find_by(id:params[:id])
+  end
+
   private
   def post_params
     params.require(:post).permit(:content,:image).merge(user_id:current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts, only:[:index,:new,:create]
+  resources :posts, only:[:index,:new,:create,:show]
   resources :users, only:[:new,:create,:show,:edit,:update] do
     member do
       post "image_update"
