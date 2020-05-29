865137acaf92bef20741482288d6da201f80bf6a
Haruka Teduka <th_kiyose@outlook.com>
2020-05-29 12:47:07 +0900

    バリデーション修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
-  validates :content, presence:true, length: { maximum: 200 }
+  validates :content, length: { maximum: 200 }
+  validates :image, presence:true
   mount_uploader :image, ImageUploader
 end
