75d91dd57c20e40d9b158482b10d0c51b6ce4e46
Haruka Teduka <th_kiyose@outlook.com>
2020-06-01 10:47:23 +0900

    エラーの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "InstagramClone_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :letter_opener_web
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
@@ -92,6 +94,3 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-
-config.action_mailer.default_url_options = { host: 'localhost:3000' }
-config.action_mailer.delivery_method = :letter_opener_web
